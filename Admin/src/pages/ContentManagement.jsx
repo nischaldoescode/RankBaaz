14992a6d533a046573c10d49cc6a65bef0279702
Nischaldoescode <nischala389@gmail.com>
2025-10-24 22:44:31 +0530

    Added Dynamic Sitemap

--- Admin/src/pages/ContentManagement.jsx
+++ Admin/src/pages/ContentManagement.jsx
@@ -2533,7 +2533,7 @@ const ContentManagement = () => {
                           }))
                         }
                         className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
-                        placeholder="© {year} TestMaster Pro. All rights reserved."
+                        placeholder="© {year} RankBaaz Pro. All rights reserved."
                       />
                     </div>
                   </div>
