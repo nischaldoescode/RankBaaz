e0913130a30b09fae05a59757b92eafaea074cc9
Nischaldoescode <nischala389@gmail.com>
2026-04-20 01:07:30 +0530

    Added Teacher Dasboard Page in admin panel in side bar as well and fixed import

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, useLocation } from "react-router-dom";
 import App from "./App.jsx";
