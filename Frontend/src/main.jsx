83eaf4f0f23abfe80e90f8317b9db2b3128ebf0d
Nischaldoescode <nischala389@gmail.com>
2026-04-20 01:01:45 +0530

    Added Teacher Dasboard Page in admin panel in side bar as well and fixed import

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { CourseProvider } from "./context/CourseContext.jsx";
 import { TestProvider } from "./context/TestContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import "./styles/globals.css";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+  return null;
+};
 
 // Register Service Worker for image caching
 if ("serviceWorker" in navigator) {
@@ -49,11 +57,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
                 v7_relativeSplatPath: true,
               }}
             >
+              <ScrollToTop />
               <App />
             </BrowserRouter>
           </TestProvider>
         </CourseProvider>
       </AuthProvider>
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
